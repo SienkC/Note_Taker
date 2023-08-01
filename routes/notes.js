227116ee50e9dd1895421a60b04cc9ff31f97e35
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-31 21:35:44 -0400

    Added ability to read files to notes api

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,29 @@
 const notes = require('express').Router();
 
-// require helper functions
+// get fs functions from helpers folder
+const { readFrom, overWrite, addTo } = require('../helpers/fsHelper');
 
 // get notes
 notes.get('/', (req, res) => {
-    // add function to read .json file
+    // read the data from db file
+    readFrom('./db/db.json').then((data) => {
+        const parsedData = JSON.parse(data);
+        // display the data
+        res.json(parsedData);
+    })
 });
 
 // post new note
 notes.post('/', (req, res) => {
     // add function to add to .json file
+
+    // // get info to add from request
+    // const { title, text } = req.body;
+
+    // // if there is a body
+    // if(req.body) {
+
+    // }
 })
 
 
