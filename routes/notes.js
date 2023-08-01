367c2c29d39711bdc8798859ef37b89e28edfdd1
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-31 20:54:30 -0400

    Added base code to notes.js

---
+++ routes/notes.js
@@ -0,0 +1,16 @@
+const notes = require('express').Router();
+
+// require helper functions
+
+// get notes
+notes.get('/', (req, res) => {
+    // add function to read .json file
+});
+
+// post new note
+notes.post('/', (req, res) => {
+    // add function to add to .json file
+})
+
+
+module.exports = notes;
