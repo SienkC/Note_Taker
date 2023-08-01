b981df1d393f7514f528a595e8866328b30b4903
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-31 21:42:04 -0400

    Added post function to notes api

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const notes = require('express').Router();
 
 // get fs functions from helpers folder
-const { readFrom, overWrite, addTo } = require('../helpers/fsHelper');
+const { readFrom, addTo } = require('../helpers/fsHelper');
 
 // get notes
 notes.get('/', (req, res) => {
@@ -17,14 +17,26 @@ notes.get('/', (req, res) => {
 notes.post('/', (req, res) => {
     // add function to add to .json file
 
-    // // get info to add from request
-    // const { title, text } = req.body;
+    // get info to add from request
+    const { title, text } = req.body;
 
-    // // if there is a body
-    // if(req.body) {
+    // if there is a body
+    if(req.body) {
+        const note = {
+            title,
+            text
+        };
 
-    // }
-})
+        // add new note to notes db
+        addTo('./db/db.json', note);
+        
+        // show successful addition note
+        res.json('Note was added');
+    }
+    else {
+        res.error('Could not add note');
+    }
+});
 
 
 module.exports = notes;
